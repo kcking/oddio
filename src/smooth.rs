cde13d3cedad387ce7b9601ff3d6da07dd508e1f
Kevin King <4kevinking@gmail.com>
2022-02-10 19:51:13 -0800

    fix behavior of smooth when being set many times

--- src/smooth.rs
+++ src/smooth.rs
@@ -58,9 +58,20 @@ impl<T> Smoothed<T> {
     where
         T: Interpolate,
     {
-        self.prev = self.get();
-        self.next = value;
-        self.progress = 0.0;
+        //  IDEA: if we haven't reached destination yet and value is in same
+        //  direction, don't set progress to zero. instead draw line between
+        //  self.prev and new value, then set progress based on current progress
+        //  value converted to this line
+
+        if self.progress < 1. && (value - self.get()).sign() == (value - self.prev).sign() {
+            let current = self.get();
+            self.next = value;
+            self.progress = ((current - self.prev) / (self.next - current)).to_f32();
+        } else {
+            self.prev = self.get();
+            self.next = value;
+            self.progress = 0.0;
+        }
     }
 
     /// Get the current value
@@ -78,9 +89,17 @@ impl<T> Smoothed<T> {
 }
 
 /// Types that can be linearly interpolated, for use with [`Smoothed`]
-pub trait Interpolate {
+pub trait Interpolate:
+    core::ops::Sub<Output = Self> + core::ops::Div<Output = Self> + Sized + Copy + Clone
+{
     /// Interpolate between `self` and `other` by `t`, which should be in [0, 1]
     fn interpolate(&self, other: &Self, t: f32) -> Self;
+
+    /// Signum
+    fn sign(&self) -> f32;
+
+    /// convert to float
+    fn to_f32(&self) -> f32;
 }
 
 impl Interpolate for f32 {
@@ -88,4 +107,28 @@ impl Interpolate for f32 {
         let diff = other - self;
         self + t * diff
     }
+
+    fn sign(&self) -> f32 {
+        if self.is_sign_positive() {
+            1.
+        } else {
+            -1.
+        }
+    }
+
+    fn to_f32(&self) -> f32 {
+        *self
+    }
+}
+
+#[test]
+fn repeated_set() {
+    let mut s = Smoothed::new(0f32);
+
+    for _ in 0..1000 {
+        s.set(1.);
+        s.advance(0.001);
+    }
+
+    assert!((s.get() - 1.).abs() < 0.01);
 }
